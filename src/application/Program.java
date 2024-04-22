92807b9f62d6271bead681d5e5c3a15e26ab600a
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-04-22 15:40:08 -0300

    First clase: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Position p1 = new Position(3, 5);
+        System.out.println(p1);
     }
 }
